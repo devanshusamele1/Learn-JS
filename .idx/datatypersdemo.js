95429dd649590bcd867dcf0999b9b1eddcb0d723
devanshusamele921 <devanshusamele921@gmail.com>
2025-07-29 05:18:49 +0000

    added variables

---
+++ .idx/datatypersdemo.js
@@ -0,0 +1,7 @@
+let productPrice = 150000;
+
+let productDiscount = 15;
+
+let productName = "Apple Iphone 11 pro max(512 GB,Blue)";
+
+let isAvailable = true;
---
+++ .idx/variablename.js
@@ -0,0 +1,9 @@
+let first$name = "Devanshu";
+
+console.log(first$name);
+
+let let =10;
+
+let phone1= 9999;
+
+let 1phone = 230;
