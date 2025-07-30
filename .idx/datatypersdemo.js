813b735e207e5121708413d6057c2f060b6da2d6
devanshusamele921 <devanshusamele921@gmail.com>
2025-07-30 05:07:13 +0000

    assignment and arithmetic operators added

--- .idx/datatypersdemo.js
+++ .idx/datatypersdemo.js
@@ -5,3 +5,19 @@ let productDiscount = 15;
 let productName = "Apple Iphone 11 pro max(512 GB,Blue)";
 
 let isAvailable = true;
+
+let minusZero = -0;
+
+console.log(minusZero);
+
+let x ;
+
+let y = null;
+
+console.log(x);
+
+console.log(y);
+
+let veryBigNumber = 9876543234567865434876543223467n;
+
+console.log(veryBigNumber);
---
+++ .idx/objectdemo.js
@@ -0,0 +1,16 @@
+let product = {
+    name: "Apple Iphone 11 pro max (512 gb, Blue)",
+    price: 150000,
+    discount: 15,
+    isAvailable: false
+};
+
+let socialHandles = {
+    fb: 9000,
+    insta: 6700,
+    "linkedin": 2000,
+    x: 5000,
+    insta: 9000
+};
+
+console.log(socialHandles);
